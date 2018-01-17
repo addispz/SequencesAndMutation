a6acfa15ec3cbda11256507fab2b990831185422
Patrick Addis <addispz@rose-hulman.edu>
2018-01-17 16:30:38 -0500

    m6

--- src/m6_mutation.py
+++ src/m6_mutation.py
@@ -4,8 +4,8 @@ In this module, you mutate by CHANGING elements of a list.
 
 Authors: David Mutchler, Amanda Stouder, Chandan Rupakheti, Katie Dion,
          Claude Anderson, Delvin Defoe, Curt Clifton, their colleagues,
-         and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         and Patrick Addis.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 # ----------------------------------------------------------------------
 # STUDENTS:
@@ -116,7 +116,6 @@ def check_argument(actual_argument_value, correct_argument_value):
 
         return False
 
-
 def RETURN_replace_negatives_by_zeros(numbers):
     """
     RETURNs a NEW list that is the same as the given list of numbers,
@@ -129,10 +128,17 @@ def RETURN_replace_negatives_by_zeros(numbers):
 
     Precondition: The argument is a list of numbers.
     """
-    # TODO: 2. First, READ THE ABOVE TEST CODE.
+    # DONE: 2. First, READ THE ABOVE TEST CODE.
     #          Make sure that you understand it.
     #          Then, IMPLEMENT and test THIS FUNCTION
     #          (using the above code for testing).
+    no_zeros = []
+    for k in range (len(numbers)):
+        if numbers[k] < 0:
+            no_zeros.append(0)
+        else:
+            no_zeros.append(numbers[k])
+    return no_zeros
 
 
 def run_test_MUTATE_replace_negatives_by_zeros():
@@ -185,11 +191,15 @@ def MUTATE_replace_negatives_by_zeros(numbers):
 
     Precondition: The argument is a list of numbers.
     """
-    # TODO: 3. First, READ THE ABOVE TEST CODE.
+    # DONE: 3. First, READ THE ABOVE TEST CODE.
     #          Make sure that you understand it.
     #          Then, IMPLEMENT and test THIS FUNCTION
     #          (using the above code for testing).
+    for k in range(len(numbers)):
+        if numbers[k] < 0:
+            numbers[k] = 0
 
+            
 # ----------------------------------------------------------------------
 # If this module is running at the top level (as opposed to being
 # imported by another module), then call the 'main' function.
